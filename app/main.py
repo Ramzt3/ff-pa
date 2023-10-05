20f850449ea21804a36a5367ae2c4eb070c8d491
siliphong <it@BOOKNEAs-Mac-mini.local>
2023-10-05 17:05:12 +0700

    add sqlalchemy

--- app/main.py
+++ app/main.py
@@ -1,11 +1,34 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from .routers import item
+import time
+from sqlalchemy.orm import Session
+from . import models
+from .database import engine, get_db
 
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+
 app.include_router(item.router)
+
+import psycopg2
+from psycopg2.extras import RealDictCursor
+while True:
+    try:
+        conn = psycopg2.connect(host='localhost', database='ff-pa', user='postgres', password='000001', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was successfull!")
+        break
+    except Exception as error:
+        print("Connecting to database failed")
+        print("Error: ", error)
+        time.sleep(2)
 
 @app.get("/")
 def read_root():
     return {"data": "Hello"}
+
+@app.get("/sql")
+def test_post(db: Session = Depends(get_db)):
+    return {"good one lol"}
