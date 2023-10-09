661a1068b5e7a9abad4539513f9485f4c22147fd
siliphong <it@BOOKNEAs-Mac-mini.local>
2023-10-09 17:13:30 +0700

    remove static data

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -10,14 +10,6 @@ router = APIRouter(
     tags=["Posts"]
 )
 
-my_posts = [{"title": "title of post 1", "content": "content 1", "id": 1},
-            {"title": "ok post2", "content": "content 2", "id": 2}]
-
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
-
 @router.get("", status_code=status.HTTP_200_OK)
 def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
